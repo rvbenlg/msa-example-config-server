dfa2e65379d66bb4d6d42c8f2b11b5cac8ee4e69
rvbenlg <rulogo94@icloud.com>
2021-09-23 09:48:57 +0200

    First commit

---
+++ src/main/java/com/rvbenlg/springcloudnetflixeurekaconfigserver/SpringCloudNetflixEurekaConfigServerApplication.java
@@ -0,0 +1,15 @@
+package com.rvbenlg.springcloudnetflixeurekaconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SpringCloudNetflixEurekaConfigServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudNetflixEurekaConfigServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rvbenlg/springcloudnetflixeurekaconfigserver/SpringCloudNetflixEurekaConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rvbenlg.springcloudnetflixeurekaconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudNetflixEurekaConfigServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
